d812a2fef37039c3ccd4c967d66ac921e86aec0b
quarbby <lynnette-ng@outlook.com>
2015-10-12 19:25:03 +0800

    Made more plots

--- plotting.py
+++ plotting.py
@@ -27,17 +27,37 @@ plt.ylabel('Number of Delayed Flights')
 plt.xlabel('Origin Airport')
 plt.xticks(rotation=-30)
 plt.show()
-
-
-grouped_by_origin = df[['IsDelayed', 'Origin']].groupby('Origin').sum()
-gdf = pd.DataFrame(grouped_by_origin)
-gdf = gdf.sort(['IsDelayed'], ascending=False)
-gdf[:20].plot(kind='bar')
-plt.xticks(rotation=-30)
-
-plt.figure()
 '''
 
+grouped_by_delay = df[['ArrDelay', 'DepDelay', 'WeatherDelay', 'NASDelay', 'SecurityDelay', \
+             'CarrierDelay', 'LateAircraftDelay', 'Origin']].groupby('Origin').mean()
+gdelay = pd.DataFrame(grouped_by_delay)
+gdelay['TotalDelay'] = gdelay.ArrDelay + gdelay.DepDelay + gdelay.CarrierDelay + \
+                        gdelay.WeatherDelay + gdelay.NASDelay + gdelay.SecurityDelay + \
+                        gdelay.LateAircraftDelay
+gdelay = gdelay.sort(['TotalDelay'], ascending=False)[:20]
+gdelay = gdelay.drop(['TotalDelay'], axis=1)
+
+py.image.save_as({
+    'data': [Bar(
+        x=gdelay.index, 
+        y=gdelay[col],
+        name=col
+        ) for col in gdelay.columns],
+    'layout': Layout(
+        barmode='stack',
+        xaxis={'title': 'Origin Airport Code'},
+        yaxis={'title': 'Delay in Minutes'},
+        title='Top 20 most delayed Airports'
+    )
+}, filename='delayedairports_2.png')
+
+#gdf[:20].plot(kind='bar')
+#plt.xticks(rotation=-30)
+#plt.figure()
+
+
+# Grouped by delay
 grouped_by_delay = df[['TotalDelay', 'ArrDelay', 'DepDelay', 'WeatherDelay', \
 					  'NASDelay', 'SecurityDelay', 'LateAircraftDelay', 'Origin']].groupby('Origin').mean()
 gdelay = pd.DataFrame(grouped_by_delay)
@@ -57,3 +77,69 @@ py.image.save_as({
 		title='Top 20 most delayed Airports'
 	)
 }, filename='delayedairports_2.png')
+
+
+# Group by day of month
+grouped_by_month = df[['IsDelayed', 'DayofMonth']].groupby('DayofMonth').sum()
+gmonth = pd.DataFrame(grouped_by_month)
+
+py.image.save_as({
+    'data': [Bar(
+        x=gmonth.index, 
+        y=gmonth['IsDelayed']
+        )],
+    'layout': Layout(
+        xaxis={'title': 'Day of Month'},
+        yaxis={'title': 'Number of Delays'},
+        title='Number of Delays by Day of Month in 2007'
+    )
+}, filename='dayofmonth_delays.png')
+
+# Group by month
+grouped_by_month = df[['IsDelayed', 'Month']].groupby('Month').sum()
+gmonth = pd.DataFrame(grouped_by_month)
+
+py.image.save_as({
+    'data': [Bar(
+        x=gmonth.index, 
+        y=gmonth['IsDelayed']
+        )],
+    'layout': Layout(
+        xaxis={'title': 'Month'},
+        yaxis={'title': 'Number of Delays'},
+        title='Number of Delays by Month in 2007'
+    )
+}, filename='month_delays.png')
+
+# Group by day of week
+grouped_by_week = df[['IsDelayed', 'DayOfWeek']].groupby('DayOfWeek').sum()
+gweek = pd.DataFrame(grouped_by_week)
+
+py.image.save_as({
+    'data': [Bar(
+        x=gweek.index, 
+        y=gweek['IsDelayed']
+        )],
+    'layout': Layout(
+        xaxis={'title': 'Day of Week'},
+        yaxis={'title': 'Number of Delays'},
+        title='Number of Delays by Day of Week in 2007'
+    )
+}, filename='week_delays.png')
+
+# Group by Unique Carrier 
+grouped_by_carrier = df[['IsDelayed', 'UniqueCarrier']].groupby('UniqueCarrier').sum()
+gcarrier = pd.DataFrame(grouped_by_carrier)
+gcarrier = gcarrier.sort(['IsDelayed'], ascending=False)
+
+py.image.save_as({
+    'data': [Bar(
+        x=gcarrier.index, 
+        y=gcarrier['IsDelayed']
+        )],
+    'layout': Layout(
+        xaxis={'title': 'Unique Carriers'},
+        yaxis={'title': 'Number of Delays'},
+        title='Top 20 most number of delays Carriers in 2007'
+    )
+}, filename='carrier_delays.png')
