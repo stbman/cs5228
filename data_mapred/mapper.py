11eb00b6e1a3bf98d5ff6c662b97b005d1730542
quarbby <lynnette-ng@outlook.com>
2015-10-22 10:13:40 +0800

    Fixed mapper and reducer

--- data_mapred/mapper.py
+++ data_mapred/mapper.py
@@ -6,24 +6,41 @@ import numpy as np
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import GradientBoostingRegressor
 
-file_name = '2007_weather_preprocess.csv'
+file_name = '2007_weather_short.csv'
 window = 50
+k = window/2    # Thought windows should be of a ratio
 
 iter_csv = pd.read_csv(file_name, iterator=True, chunksize=window)
 
+knn = KNeighborsClassifier(n_neighbors=k)
+
+# To change classifiers 
 gbr = GradientBoostingRegressor(n_estimators=20, learning_rate=0.1,
                       max_depth=1, random_state=0, loss='ls')
 import numpy as np
+
 for chunk in iter_csv:
     chunk = chunk.drop(['DateTime'], axis=1)
+    
     attributes = chunk.columns.values
+    attributes = np.delete(attributes, attributes.tolist().index('TotalDelay'))    
+    
     training_chunk = chunk.iloc[:-1]
     predict_chunk = chunk.iloc[-1]
     actual = predict_chunk['TotalDelay']
     predict_chunk = predict_chunk.drop(['TotalDelay'])
-    attributes = np.delete(attributes, attributes.tolist().index("TotalDelay"))
-    gbr.fit(training_chunk[attributes], training_chunk['TotalDelay'])
+        
+    # KNN
+    knn.fit(training_chunk[attributes], training_chunk['TotalDelay'])
+    neighbours = knn.kneighbors(predict_chunk[attributes], return_distance=False).flatten()
+    
+    # Other classifiers
+    new_training_chunk = training_chunk.ix[neighbours]
+
+    gbr.fit(new_training_chunk[attributes], new_training_chunk['TotalDelay'])
     prediction = gbr.predict(predict_chunk)[0]
+    
     print prediction, actual
+
 
 
--- data_mapred/reducer.py
+++ data_mapred/reducer.py
@@ -1,12 +1,31 @@
 #!/usr/bin/python
 
 import sys
+import pandas as pd
+import numpy as np
 
-sum = 0
-count = 0
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import precision_recall_fscore_support
+
+df = pd.DataFrame(columns=['actual', 'predicted'])
+
 for line in sys.stdin:
     predicted, actual = line.strip().split(" ")
-    count += 1
-    sum += (float(actual) - float(predicted))**2
+    df = df.append({'actual': actual, 'predicted': predicted}, ignore_index=True)
 
-print sum/count
+rmse = np.sqrt(mean_squared_error(df['actual'], df['predicted']))
+
+df['actualBoolean']    = df['actual'].map(lambda x: 1 if x > 15 else 0)    
+df['predictedBoolean'] = df['predicted'].map(lambda x: 1 if x > 15 else 0)    
+
+#print df
+
+
+prec_recall_score   = precision_recall_fscore_support(df['actualBoolean'], df['predictedBoolean'])
+prec_delayed        = prec_recall_score[0][1]
+recall_delayed      = prec_recall_score[1][1]
+fscore_delayed      = prec_recall_score[2][1]
+num_corr            = prec_recall_score[3][1]
+    
+print rmse, prec_delayed, recall_delayed, fscore_delayed, num_corr
+
